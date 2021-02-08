5a1da0ae44588071d1c8af5a3e17da988d15c134
daigoyb <rafaeldaigo.yb@gmail.com>
2021-02-08 18:58:45 -0300

    html form login added -- ainda falta css

---
+++ script/login.js
@@ -0,0 +1,62 @@
+
+const BASE_URL = "accenture-java-desafio.herokuapp.com/";
+const CONTENT = "application/json";
+const MODE = "cors";
+const LOCALHOST = "localhost:5500/"
+
+
+
+
+
+async function login(user, senha){
+    try {
+        const response = await fetch(`${BASE_URL}altera-senha`, {
+            method: "POST",
+            body: JSON.stringify({
+                senha: senha,
+                usuario: user,
+            }),
+            headers: {
+                'Content-type': CONTENT
+            }
+        })
+        .then( res => {
+            if (res.ok){
+                location.replace(`${LOCALHOST}dashboard.html`);
+                const resData = res.json()
+                console.log(resData);
+                
+            }
+            location.replace(`${LOCALHOST}error.html`)
+            return Promise.reject(res)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function alterarSenha(user, senha){
+    try {
+        const response = await fetch(`${BASE_URL}altera-senha`, {
+            method: "POST",
+            body: JSON.stringify({
+                senha: senha,
+                usuario: user,
+            }),
+            headers: {
+                'Content-type': CONTENT
+            }
+        })
+        .then( res => {
+            if (res.ok){
+                console.log(res.json());
+                location.replace(`${LOCALHOST}login.html`);
+            }
+            location.replace(`${LOCALHOST}error.html`)
+            return Promise.reject(res)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
